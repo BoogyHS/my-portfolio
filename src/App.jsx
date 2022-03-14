b2c7705e3fe0631beee2734d63f5d27df8c131cd
Bozhidar <bozhidar.atanasov@atos.net>
2022-03-14 22:33:34 +0200

    redux added to the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer'
+import AddTodo from './components/AddTodo'
+import VisibleTodoList from './components/VisibleTodoList'
+
+const App = () => {
+	return (
+		<div>
+			<AddTodo />
+			<VisibleTodoList />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
